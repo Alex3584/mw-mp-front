71fabc308afb11f7b6e57f43fc1d84f47e364aa0
Alex3584 <kolesnyk05071984@gmail.com>
2024-06-19 21:09:12 +0300

    Fixed error ProductDetailsPage

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,14 +1,13 @@
-import { Helmet } from 'react-helmet'
+import { Helmet } from "react-helmet";
 
 const ProductDetailsPage: React.FC = () => {
-  return
+  return (
     <>
       <Helmet>
-        <title>Страница продукта - MW Marketplace</title>
+        <title>Product Details</title>
       </Helmet>
-
-      <h1>Страница продукта</h1>
     </>
-}
+  );
+};
 
 export default ProductDetailsPage;
--- src/pages/ProductDetailsPage/index.ts
+++ src/pages/ProductDetailsPage/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./PoductDetailsPage"
+export { default } from './ProductDetailsPage'
